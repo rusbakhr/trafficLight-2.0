6ff7422c7928898e5630583b7816263f7a4e86fd
Rustam <--->
2022-10-23 21:30:05 +0500

    Initial Commit

---
+++ trafficLight 2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLight 2.0
+//
+//  Created by Rustam Bakhritdinov on 23.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
